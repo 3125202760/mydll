ef9491f61eebf91c476d4d2101ea24879b5cfd18
Franklyn Deckow <111890725+3125202760@users.noreply.github.com>
2023-05-21 18:14:23 +0800

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,7 +1,19 @@
 #include <lua.hpp>
 
-extern "C" {
-    __declspec(dllexport) double add(double a, double b) {
-        return a + b;
+    static int l_add(lua_State *L) {
+    double a = luaL_checknumber(L, 1);
+    double b = luaL_checknumber(L, 2);
+    lua_pushnumber(L, a + b);
+    return 1;
     }
+
+    extern "C" {
+    __declspec(dllexport) int luaopen_mydll(lua_State *L) {
+        luaL_Reg reg[] = {
+            {"add", l_add},
+            {NULL, NULL}
+        };
+        luaL_register(L, "mydll", reg);
+        return 1;
+        }
 }
