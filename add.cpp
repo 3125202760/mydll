046a80d679f3951fd3308e52bd02bfaa0044871d
Franklyn Deckow <111890725+3125202760@users.noreply.github.com>
2023-05-21 18:20:08 +0800

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,19 +1,20 @@
-#include <lua.hpp>
+extern "C" {
+#include "lua.h"
+#include "lauxlib.h"
+}
 
-    static int l_add(lua_State *L) {
+static int l_add(lua_State *L) {
     double a = luaL_checknumber(L, 1);
     double b = luaL_checknumber(L, 2);
     lua_pushnumber(L, a + b);
     return 1;
-    }
+}
 
-    extern "C" {
-    __declspec(dllexport) int luaopen_mydll(lua_State *L) {
-        luaL_Reg reg[] = {
-            {"add", l_add},
-            {NULL, NULL}
-        };
-        luaL_register(L, "mydll", reg);
-        return 1;
-        }
+extern "C" int luaopen_add(lua_State *L) {
+    luaL_Reg reg[] = {
+        { "add", l_add },
+        { NULL, NULL }
+    };
+    luaL_register(L, "add", reg);
+    return 1;
 }
