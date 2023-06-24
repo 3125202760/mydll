d10eb05382dca44ad04abb43a9f8dc4ad675bb53
Clyde Smith <111890725+3125202760@users.noreply.github.com>
2023-06-24 22:44:56 +0800

    Update mylib.cpp

--- mylib.cpp
+++ mylib.cpp
@@ -1,54 +1,22 @@
-#include <Windows.h>
-#pragma comment(lib, "User32.lib")
+#include <windows.h>
 
-__declspec(dllexport) void copy_and_paste(const char* str)
-{
-    // 打开剪贴板
-    if (!OpenClipboard(NULL))
-        return;
-
-    // 清空剪贴板
-    EmptyClipboard();
-
-    // 分配内存
-    HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, strlen(str) + 1);
-    if (hMem == NULL)
-    {
-        CloseClipboard();
-        return;
+extern "C" {
+    __declspec(dllexport) void copyText() {
+        keybd_event(VK_CONTROL, 0, 0, 0);
+        keybd_event('C', 0, 0, 0);
+        keybd_event('C', 0, KEYEVENTF_KEYUP, 0);
+        keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
     }
 
-    // 锁定内存
-    char* pMem = (char*)GlobalLock(hMem);
-    if (pMem == NULL)
-    {
-        GlobalFree(hMem);
-        CloseClipboard();
-        return;
+    __declspec(dllexport) void pasteText() {
+        keybd_event(VK_CONTROL, 0, 0, 0);
+        keybd_event('V', 0, 0, 0);
+        keybd_event('V', 0, KEYEVENTF_KEYUP, 0);
+        keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
     }
 
-    // 复制字符串到内存
-    strcpy_s(pMem, strlen(str) + 1, str);
-
-    // 解锁内存
-    GlobalUnlock(hMem);
-
-    // 设置剪贴板数据
-    SetClipboardData(CF_TEXT, hMem);
-
-    // 关闭剪贴板
-    CloseClipboard();
-
-    // 模拟Ctrl+V
-    keybd_event(VK_CONTROL, 0, 0, 0);
-    keybd_event('V', 0, 0, 0);
-    keybd_event('V', 0, KEYEVENTF_KEYUP, 0);
-    keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
-}
-
-__declspec(dllexport) void press_enter()
-{
-    // 模拟回车键
-    keybd_event(VK_RETURN, 0, 0, 0);
-    keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
+    __declspec(dllexport) void pressEnter() {
+        keybd_event(VK_RETURN, 0, 0, 0);
+        keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
+    }
 }
